6f05210b90e9e47b5b5be35c978ee56ce5ef267c
bhuuvan <68323935+bhuuvan@users.noreply.github.com>
2023-12-20 22:59:07 -0600

    Update 5-Argument-Parameter-Postional-Function-Arguments.py

--- 5-Argument-Parameter-Postional-Function-Arguments.py
+++ 5-Argument-Parameter-Postional-Function-Arguments.py
@@ -1,22 +1,23 @@
-#Positonal and Keyword Arguments
-def my_func(a,b,c):
-    print("a={0}, b={1}, c= {2}".format(a,b,c))
+# Function with positional arguments
+def my_func(a, b, c):
+    print("a={0}, b={1}, c={2}".format(a, b, c))
 
-my_func(9,26,7)
+# Calling the function with positional arguments: a=9, b=26, c=7
+my_func(9, 26, 7)
 
+# Function with default parameter
+def my_func2(a, b, c=20):
+    print("a={0}, b={1}, c={2}".format(a, b, c))
 
-#by passing a default paramter
-def my_func2(a,b,c=20):
-    print("a={0}, b={1}, c= {2}".format(a, b, c))
+# Calling the function with default value for 'c': a=10, b=2, c=20
+my_func2(10, 2)
 
-my_func2(10,2)
+# Function with all default parameters
+def my_func3(a=23, b=2323, c=2322):
+    print("a={0}, b={1}, c={2}".format(a, b, c))
 
-#by passing all parameters as default
-def my_func3(a=23,b=2323,c=2322):
-    print("a={0}, b={1}, c= {2}".format(a, b, c))
-
+# Calling the function with all default values: a=23, b=2323, c=2322
 my_func3()
 
-
-#over-writing the default parameter
-my_func3(a=111,b=2222,c=22222)
+# Overwriting default values using keyword arguments: a=111, b=2222, c=22222
+my_func3(a=111, b=2222, c=22222)
